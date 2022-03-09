f3c240500de6f87f487d12d50e3a2346a565ba0e
Hammad <hmdshfq@outlook.com>
2022-03-09 23:41:34 +0100

    create app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect, React } from 'react';
+import api from './api/swagger';
+import uuid from 'react-uuid';
+
+import SplashScreen from './components/SplashScreen';
+import HomeScreen from './components/HomeScreen';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [accessData, setAccessData] = useState(null);
+  let accessToken = '';
+    const anonUser = {
+        Device: {
+            PlatformCode: 'WEB',
+            Name: uuid(),
+        },
+    };
 
-export default App
+    useEffect(() => {
+        api.post('/Authorization/SignIn', anonUser).then(response => {
+            setAccessData(response.data);
+        });
+    }, []);
+  
+  if (!accessData) return (
+    <SplashScreen />
+  );
+  else {
+    accessToken = accessData.AuthorizationToken.Token;
+    return <HomeScreen token={accessToken} />;
+  }
+};
+
+export default App;
