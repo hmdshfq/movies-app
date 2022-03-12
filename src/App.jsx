c6a8ff9fe0ad13c0e3927c0c5f60de1feef67e2d
Hammad <hmdshfq@outlook.com>
2022-03-12 23:37:55 +0100

    Add comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import styled from 'styled-components';
 import SplashScreen from './components/SplashScreen';
 import Nav from './components/Nav';
 import MediaPlayer from './components/MediaPlayer';
-import HomeScreen from './components/HomeScreen';
+import MediaLists from './components/MediaLists';
 import Footer from './components/Footer';
 
 const App = () => {
+  /** State for saving access token for anonymous and registered user */
   const [accessToken, setAccessToken] = useState(null);
+  /** State for media Id used to fetch media content */
   const [mediaId, setMediaId] = useState(null);
+  /** State for anonymous and registered user */
   const [userType, setUserType] = useState('TRIAL');
 
+  /** Authorization parameter for anonymous users */
   const anonUser = {
     Device: {
       PlatformCode: 'WEB',
@@ -21,30 +25,35 @@ const App = () => {
     },
   };
 
+  /** Fetch authorization token for anonymous user */
   useEffect(() => {
     api
       .post('/Authorization/SignIn', anonUser)
       .then(response => {
         setAccessToken(response.data.AuthorizationToken.Token);
-        console.log('Signed in as anonymouse user');
       })
       .catch(error => {
         console.log(error);
       });
   }, []);
 
+  /** Set splash-screen while fetching authorization token */
   if (!accessToken) return <SplashScreen />;
 
+  /** Show content after fetching authorization token */
   return (
     <Wrapper>
+      {/* Component for showing logo and sign in button */}
       <Nav setAccessToken={setAccessToken} userType={userType} setUserType={setUserType} />
       <main>
+        {/* Component for displaying media player */}
         <MediaPlayer
           token={accessToken}
           mediaId={mediaId}
           userType={userType}
         />
-        <HomeScreen token={accessToken} setId={setMediaId} />
+        {/* Component for showing media lists */}
+        <MediaLists token={accessToken} setId={setMediaId} />
       </main>
       <Footer />
     </Wrapper>
