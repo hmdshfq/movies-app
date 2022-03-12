f81ef4618498e25d9bab5dca0e9e1b4d08010a30
Hammad <hmdshfq@outlook.com>
2022-03-12 22:03:56 +0100

    Add comments to main

--- src/main.jsx
+++ src/main.jsx
@@ -5,87 +5,70 @@ import App from './App';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  /*
-  1. Use a more-intuitive box-sizing model.
-  */
+  /* Use a more-intuitive box-sizing model */
   *, *::before, *::after {
     box-sizing: border-box;
   }
-  /*
-    2. Remove default margin
-  */
+  /* Remove default margin */
   * {
     margin: 0;
   }
-  /*
-    3. Allow percentage-based heights in the application
-  */
+  /* Allow percentage-based heights in the application */
   html, body {
     height: 100%;
   }
-  /*
-    Typographic tweaks!
-    4. Add accessible line-height
-    5. Improve text rendering
-  */
+  /* Typographic tweaks */
   body {
     line-height: 1.5;
     -webkit-font-smoothing: antialiased;
   }
-  /*
-    6. Improve media defaults
-  */
+  /* Improve media defaults */
   img, picture, video, canvas, svg {
     display: block;
     max-width: 100%;
   }
-  /*
-    7. Remove built-in form typography styles
-  */
+  /* Remove built-in form typography styles */
   input, button, textarea, select {
     font: inherit;
   }
-  /*
-    8. Avoid text overflows
-  */
+  /* Avoid text overflows */
   p, h1, h2, h3, h4, h5, h6 {
     overflow-wrap: break-word;
   }
-  /*
-    9. Create a root stacking context
-  */
+  /* Create a root stacking context */
   #root {
     isolation: isolate;
     height: 100%;
   }
-  /*
-    10. Colors
-  */
+  /* Global styles */
   :root{
 
+    /* Set default font */
     font-family: 'Nunito', sans-serif;
     
+    /* Set default background and text color */
     background: var(--gray-100);
     color: var(--gray-900);
 
+    /* Create gray shades */
     --gray-100: hsl(1 1% 10%);
     --gray-300: hsl(1 3% 30%); 
     --gray-500: hsl(1 5% 50%);
     --gray-700: hsl(1 7% 70%);
     --gray-900: hsl(1 9% 90%);
 
+    /* Create primary shades */
     --primary-100: hsl(1 74% 20%);
     --primary-300: hsl(1 74% 35%);
     --primary-500: hsl(1 74% 50%);
     --primary-700: hsl(1 74% 60%);
     --primary-900: hsl(7 74% 70%);
 
+    /* Set border radius */
     --border-radius: 4px;
   }
 
-  /*
-    11. Slick Slider
-  */
+  /* Add space between media cards and hover and effect */
   .slick-slide{
     height: 200px;
     display: flex !important;
@@ -103,6 +86,7 @@ const GlobalStyles = createGlobalStyle`
     margin: 0 -10px;
   }
 
+  /** Style sign in overlay modal */
   [data-reach-dialog-overlay]{
     background: hsl(0 0% 0% / 0.6);
   }
